fabe7605f37db4482d94ec01ac81984c6210a7f0
Victor Duarte <victorctii217@gmail.com>
2022-05-02 16:14:47 -0300

    creating custom hook - useFetch

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle';
 import { useCounter } from '../../hooks/useCounter';
+import { useFetch } from '../../hooks/useFetch';
 
 const UseRef = (props) => {
 
     const [count, inc, dec] = useCounter(10)
+    const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+    const response = useFetch(url)
+
+    function showStates(states) {
+        return states.map(state => <li key={state.nome}>{state.nome} - {state.sigla} </li>)
+    }
+
+    /* 
+        fetch('http://files.cod3r.com.br/curso-react/estados.json')
+            .then(resp => resp.json())
+            .then (json => console.log(json))
+    */
 
     return (
         <div className="UseCustom">
@@ -27,6 +40,13 @@ const UseRef = (props) => {
                     <button className="btn"
                     onClick={() => dec()}>-1</button>
                 </div>
+            </div>
+
+            <SectionTitle title="#02" />
+            <div className="center">
+                <ul>
+                    {!response.loading ? showStates(response.data) : false}
+                </ul>
             </div>
         </div>
     )
